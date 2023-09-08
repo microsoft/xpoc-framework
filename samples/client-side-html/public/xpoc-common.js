84c2261ee1ddbe760fd0d9e59c76b2ea0e67d649
Larry Joy <ljoy@microsoft.com>
2023-09-07 18:45:02 -0700

    Minor spelling; headers;

--- samples/client-side-html/public/xpoc-common.js
+++ samples/client-side-html/public/xpoc-common.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 function showError(errMsg) {
     errMsgElem = document.getElementById('errorMsg');
     errMsgElem.textContent = errMsg;

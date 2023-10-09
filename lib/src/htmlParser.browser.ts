8f290dda0a0fb852e9f124adf7c716d0a885b84a
Larry Joy <ljoy@microsoft.com>
2023-10-09 16:49:10 -0700

    Remove script, img elements after parsing html

--- lib/src/htmlParser.browser.ts
+++ lib/src/htmlParser.browser.ts
@@ -1,22 +1,20 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-const cache = new Map<string, Document>();
-
 export async function query(url: string, nodeQuery: string, attribute: string): Promise<string | undefined | Error> {
     const htmlOrError = await fetch(url).then((res: Response) => res.text()).catch((err: Error) => err);
     if (htmlOrError instanceof Error) return htmlOrError;
     const html: string = htmlOrError;
-    let document = cache.get(url)
-    if (!document) {
-        try {
-            document = new DOMParser().parseFromString(html, 'text/html');
-        } catch (err) {
-            console.error(err);
-            throw new Error('Failed to parse HTML');
-        }
-        cache.set(url, document);
+    const document = new DOMParser().parseFromString(html, 'text/html');
+    const errorNode = document.querySelector("parsererror");
+    if (errorNode) {
+        return new Error('Failed to parse HTML')
     }
+    // Remove all <script> and <img> elements
+    // DOMParser().parseFromString does not evaluate scripts, but it does load images
+    document.querySelectorAll('script, img').forEach(script => {
+        script.parentNode?.removeChild(script);
+    });
     const node = document.querySelector(nodeQuery) ?? undefined;
     return node?.getAttribute(attribute) ?? undefined;
 }   

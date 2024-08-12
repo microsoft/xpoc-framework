4b45a3644c161dce2fa9387b6a76c942a24329b7
Larry Joy <ljoy@microsoft.com>
2024-08-12 16:26:56 -0700

    Output raw html

--- lib/src/htmlParser.node.ts
+++ lib/src/htmlParser.node.ts
@@ -22,6 +22,9 @@ export async function query(
         Cookie: 'LOGIN_INFO=AFmmF2swRQIgDOD9dP33CouHlOMvqgGfXy7WhA-kbAqYgIQLU_UHMxgCIQDkYtstUMeDI4ApGzsieOOVa4psuBdEReLGM5nl8E6dwA:QUQ3MjNmeDBMa0hBVWFScFA0cDd1UFhiaV83QnR3ZTdOMEFaX0YxVzFaUkVSbTNJMnZEVU8zUDJvVWdXME1ZX3JSOFZ6VlJUcEJhVEpZUXFWQmdtSE5wMkRPNE5vWERoWFJUaHdGRUU1YmQ5a1ZHVXR5Zm1kTElDeTVfMzZqYV9pMnJwRVBEYkdORkEyaVBmbUJqbTZ3OWk1aTF4LXBtdGFR; HSID=Alc8IxYLnYXz5rIHN; SSID=A9Uvyb-qVOLDBcjcf; APISID=UTzoarQswHTdKJnh/ARAFqxvcjePj7dcTm; SAPISID=5fj-c8XfmfsCnF6B/AIvk9du0IJnUcULAC; __Secure-1PAPISID=5fj-c8XfmfsCnF6B/AIvk9du0IJnUcULAC; __Secure-3PAPISID=5fj-c8XfmfsCnF6B/AIvk9du0IJnUcULAC; VISITOR_PRIVACY_METADATA=CgJVUxIEGgAgUw%3D%3D; VISITOR_INFO1_LIVE=lMiUs9IOJMM; NID=515=h6wGL9xuNTNtu0VEmDDBsbeXexYV1Th7jshfNyYqYK3V-eQOyoJMWto2tITFhBp8LjrNN35qezbuNri5ISO-AMNi9n_etHd3zZeSr1IGJ_v5MAcm8FUOVubfDio46qlFMuBKeducsS82fJzatykg7_G-ldqRnnNhkEWDn-6fbix7WSJenqsil_YaQCT5nU380GFJn2GTAEIdYFbHUZqANPimYI2z1nZEzqNqQTZV7HAm2ik; VISITOR_INFO1_LIVE=lMiUs9IOJMM; VISITOR_PRIVACY_METADATA=CgJVUxIEGgAgUw%3D%3D; SID=g.a000mwgBGdKgYhaY-E6VaiX7y7-qHLBwuMwJnYHuz-pSAl2fs-YYiPADeUY7pmlzMbovqZD4HgACgYKASYSARESFQHGX2MilhAf_CpeX2AdSnIItHO41RoVAUF8yKrHrES0lhlEaO07ZjxdofS50076; __Secure-1PSID=g.a000mwgBGdKgYhaY-E6VaiX7y7-qHLBwuMwJnYHuz-pSAl2fs-YY6d9W3DbIjFhvFn9eEF1oagACgYKAfwSARESFQHGX2MinyUm48ngUnKNj5Yk8QW_BxoVAUF8yKr8kE6905Mb5jUdIumUSiHC0076; __Secure-3PSID=g.a000mwgBGdKgYhaY-E6VaiX7y7-qHLBwuMwJnYHuz-pSAl2fs-YYKzY0aZahPDyuInl2gKPibwACgYKAbgSARESFQHGX2MiZtc_eB0g4bOFqkxk7qEkLBoVAUF8yKohIcuCRquKFzxdiDbtUY7e0076; PREF=f6=40000000&f7=4140&tz=America.Los_Angeles&f5=20000&f4=4000000; YSC=PVm6-O8vkps; YT_CL={"loctok":"ACih6ZOBUHQTC631OBJeV6CydAiYtFepAClWOsgYCsYIWcp512D0445VGQCEXPJHYdNumW8Pf-D_mGwFyJSCQFGzgoVle0AQi84"}; __Secure-1PSIDTS=sidts-CjIBUFGoh3g_GCCXNBXNwQrwuyMQT0BCIBkG9-deVjakWO0SMN_SBjrV3hPpLfUwsCG6bBAA; __Secure-3PSIDTS=sidts-CjIBUFGoh3g_GCCXNBXNwQrwuyMQT0BCIBkG9-deVjakWO0SMN_SBjrV3hPpLfUwsCG6bBAA; SIDCC=AKEyXzU5xB9jDoEBFiuIiYrOg5KNsoWKn7N1-vZUWniMqXmSoSkrAE7_auqbRpwcxmJs_hXR_HM; __Secure-1PSIDCC=AKEyXzUMQBrKzmV3y2XVAhfGcSe9H-8y40RDJOPySL1wCSd4UNDkQUNEYUVK7S792bmZMwN4QtQ; __Secure-3PSIDCC=AKEyXzU9Yiga1ppUTEGoAV8pIKBeMQSnQ2jMM5XaCwOKH48hFrrTRihsOQCygtIq1AsaEGZJZZQ0',
         'upgrade-insecure-requests': '1',
         Dnt: '1',
+        'Cache-Control': 'max-age=0',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'en-US,en;q=0.9',
     };
 
     const htmlOrError = await fetch(url, { headers })
@@ -43,5 +46,8 @@ export async function query(
             results.push(queryResult.html() ?? undefined);
         }
     }
+    console.info('--------------------------------------------------------------------');
+    console.info(html);
+    console.info('--------------------------------------------------------------------');
     return results;
 }
